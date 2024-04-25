7039027c0a4e142a1519c19323bd8e15b7c2822f
Jacob DiBiase <jakesback93@hotmail.com>
2024-04-24 23:22:56 -0700

    Fix index and documentation.html and parse incoming requests

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+// Import the http, url, and fs modules
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
-http.createServer((request, response) => {
+// Create an http server using the createServer method of the http module
+http
+  .createServer((request, response) => {
+    // Extract the URL requested by the client
+    let requestedUrl = request.url;
+    let filePath = "";
 
-}),listen(8080);
+    if (requestedUrl.includes("documentation.html")) {
+      filePath = "./documentation.html";
+    } else {
+      filePath = "./index.html";
+    }
+  })
+  .listen(8080);
