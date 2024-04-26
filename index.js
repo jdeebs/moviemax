7a2bba930e891566a0120929cd33a99149b9e319
Jacob DiBiase <jakesback93@hotmail.com>
2024-04-26 13:12:52 -0700

    Update index.js to use Express, add movie data, and state GET requests

--- index.js
+++ index.js
@@ -1,23 +1,86 @@
-const express = require("express"),
-  morgan = require("morgan");
-(fs = require("fs")), (path = require("path"));
-
+const express = require("express");
 const app = express();
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
-  flags: "a",
+let topMovies = [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    director: "Frank Darabont",
+    cast: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
+    imdbRating: 9.3,
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    director: "Francis Ford Coppola",
+    cast: ["Marlon Brando", "Al Pacino", "James Caan"],
+    imdbRating: 9.2,
+  },
+  {
+    title: "The Godfather: Part II",
+    year: 1974,
+    director: "Francis Ford Coppola",
+    cast: ["Al Pacino", "Robert De Niro", "Robert Duvall"],
+    imdbRating: 9.0,
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    cast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    imdbRating: 9.0,
+  },
+  {
+    title: "12 Angry Men",
+    year: 1957,
+    director: "Sidney Lumet",
+    cast: ["Henry Fonda", "Lee J. Cobb", "Martin Balsam"],
+    imdbRating: 8.9,
+  },
+  {
+    title: "Schindler's List",
+    year: 1993,
+    director: "Steven Spielberg",
+    cast: ["Liam Neeson", "Ralph Fiennes", "Ben Kingsley"],
+    imdbRating: 8.9,
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+    director: "Peter Jackson",
+    cast: ["Elijah Wood", "Viggo Mortensen", "Ian McKellen"],
+    imdbRating: 8.9,
+  },
+  {
+    title: "Pulp Fiction",
+    year: 1994,
+    director: "Quentin Tarantino",
+    cast: ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+    imdbRating: 8.9,
+  },
+  {
+    title: "The Good, the Bad and the Ugly",
+    year: 1966,
+    director: "Sergio Leone",
+    cast: ["Clint Eastwood", "Eli Wallach", "Lee Van Cleef"],
+    imdbRating: 8.8,
+  },
+  {
+    title: "Fight Club",
+    year: 1999,
+    director: "David Fincher",
+    cast: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
+    imdbRating: 8.8,
+  },
+];
+
+// GET requests
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
-app.use(morgan("combined", { stream: accessLogStream }));
-
-app.get("/", (req, res) => {
-  res.send("Welcome to my app!");
+app.get('/', (req, res) => {
+    res.send('Welcome to MovieMax!')
 });
 
-app.get("/secreturl", (req, res) => {
-  res.send("This is a secret url with super top-secret content.");
-});
-
-app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
-});
+app.use(express.static('public'));
