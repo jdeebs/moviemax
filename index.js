094b61bdcb7f298b5620c3a569a2805ab616d73f
Jacob DiBiase <jakesback93@hotmail.com>
2024-04-30 11:36:17 -0700

    Add CREATE req for new user

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
 
+// Create an array of objects for users
+let users = [];
+
 // Create an array of objects for top 10 movies data
 let topMovies = [
   {
@@ -22,7 +25,8 @@ let topMovies = [
     imdbRating: 9.3,
     genre: {
       name: "Drama",
-      description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+      description:
+        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51DPJlHuWgL._AC_.jpg",
   },
@@ -34,7 +38,8 @@ let topMovies = [
     imdbRating: 9.2,
     genre: {
       name: "Crime",
-      description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+      description:
+        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51ZBnnNzvyL._AC_.jpg",
   },
@@ -46,7 +51,8 @@ let topMovies = [
     imdbRating: 9.0,
     genre: {
       name: "Crime",
-      description: "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate."
+      description:
+        "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/41t2+cWZizL._AC_.jpg",
   },
@@ -58,7 +64,8 @@ let topMovies = [
     imdbRating: 9.0,
     genre: {
       name: "Action",
-      description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+      description:
+        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51Lu09Yi8rL._AC_.jpg",
   },
@@ -70,7 +77,8 @@ let topMovies = [
     imdbRating: 8.9,
     genre: {
       name: "Drama",
-      description: "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence."
+      description:
+        "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51aAwtB1GiL._AC_.jpg",
   },
@@ -82,7 +90,8 @@ let topMovies = [
     imdbRating: 8.9,
     genre: {
       name: "Biography",
-      description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis."
+      description:
+        "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/61agSwaBqVL._AC_.jpg",
   },
@@ -94,7 +103,8 @@ let topMovies = [
     imdbRating: 8.9,
     genre: {
       name: "Adventure",
-      description: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."
+      description:
+        "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/81UWg+0DZtL._AC_.jpg",
   },
@@ -106,7 +116,8 @@ let topMovies = [
     imdbRating: 8.9,
     genre: {
       name: "Crime",
-      description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
+      description:
+        "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51UWgWpW3IL._AC_.jpg",
   },
@@ -118,7 +129,8 @@ let topMovies = [
     imdbRating: 8.8,
     genre: {
       name: "Western",
-      description: "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery."
+      description:
+        "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51d5LWp39BL._AC_.jpg",
   },
@@ -130,7 +142,8 @@ let topMovies = [
     imdbRating: 8.8,
     genre: {
       name: "Drama",
-      description: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more."
+      description:
+        "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
     },
     imageUrl: "https://m.media-amazon.com/images/I/51uv6FqqtiL._AC_.jpg",
   },
@@ -141,6 +154,19 @@ app.use(morgan("combined", { stream: accessLogStream }));
 
 // Server static files from the public folder
 app.use(express.static("public"));
+
+// CREATE new user
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("Users need names");
+  }
+});
 
 // READ top 10 movies data as JSON
 app.get("/movies", (req, res) => {
@@ -174,7 +200,9 @@ app.get("/movies/genre/:genreName", (req, res) => {
 // READ movie by director
 app.get("/movies/director/:directorName", (req, res) => {
   const { directorName } = req.params;
-  const director = topMovies.find((movie) => movie.director === directorName).director;
+  const director = topMovies.find(
+    (movie) => movie.director === directorName
+  ).director;
 
   if (director) {
     res.status(200).json(director);
