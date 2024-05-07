739b06379b4ff4e578f64b5d991f9e6a1f3abf72
Jacob DiBiase <jakesback93@hotmail.com>
2024-05-07 16:12:39 -0700

    Update create new user endpoint

--- index.js
+++ index.js
@@ -174,16 +174,33 @@ app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static("public"));
 
 // CREATE new user
-app.post("/users", (req, res) => {
-  const newUser = req.body;
-
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send("Users need names");
-  }
+app.post("/users", async (req, res) => {
+  await Users.findOne({
+    Username: req.body.Username,
+  })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
 // CREATE new favorite movies by title
