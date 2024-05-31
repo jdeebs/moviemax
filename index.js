6b1109a1f7492882d973600f6ce500f6181aae06
Jacob DiBiase <jakesback93@hotmail.com>
2024-05-31 12:37:12 -0700

    Re-enable jwt token authentication in /movies endpoint

--- index.js
+++ index.js
@@ -124,6 +124,7 @@ app.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
 // READ all movie data
 app.get(
   "/movies",
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     await Movies.find()
       .then((movies) => {
