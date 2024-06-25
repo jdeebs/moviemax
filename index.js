8f8133e8a84ac0f3dc84f56692f006b79cccac80
Jacob DiBiase <jakesback93@hotmail.com>
2024-06-24 22:45:04 -0700

    Update CORS allowed origins

--- index.js
+++ index.js
@@ -17,7 +17,26 @@ app.use(express.urlencoded({ extended: true }));
 
 // Import CORS
 const cors = require("cors");
-app.use(cors());
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://localhost:1234",
+  "https://moviemax-jdeebs.netlify.app/",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // Import auth and require Passport module
 let auth = require("./auth")(app); // (app) argument ensures Express is available in auth.js
